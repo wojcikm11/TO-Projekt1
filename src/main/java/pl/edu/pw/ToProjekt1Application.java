841fdca73a3c813accaf854dcd34dfe58d87adc2
wojcikm11 <61560837+wojcikm11@users.noreply.github.com>
2021-10-11 19:08:25 +0200

    Initial Commit

---
+++ src/main/java/pl/edu/pw/ToProjekt1Application.java
@@ -0,0 +1,13 @@
+package pl.edu.pw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToProjekt1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToProjekt1Application.class, args);
+    }
+
+}
